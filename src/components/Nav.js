a94d1baac8960ff2d6fb893173289b38a73f0201
sunrun93 <wangjian9302@163.com>
2017-07-18 23:17:36 +0800

    add bind

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,15 +8,19 @@ import {Route} from 'react-router';
 let Nav = React.createClass({
     handleClick: function(event){
         console.log(event.target);
-        this.props.navTo;
+        this.props.navTo();
+        this.setState({bgColor:this.state.bgColor ==='pink'?'green':'pink'});
+    },
+    getInitialState:function(){
+        return {bgColor:'pink'}
     },
     render: function () {
         const data = this.props.data;
-        const navItems = data.map((item, index) => {
+        const navItems = data.map(function(item, index){
             return (
-                <li className='navItem' onClick={this.handleClick} key={item.id}>{item.name}</li>
+                <li className='navItem' onClick={this.handleClick} style={{backgroundColor:this.state.bgColor}} key={item.id}>{item.name}</li>
             )
-        })
+        }.bind(this))
     return(
         <div className="nav">
             <span className="navIcon"></span>
